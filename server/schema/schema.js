958c8420e1fa8f96c98c69122a4b89a7fe49e1e3
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-09 11:53:51 +0300

    Add get all movies of the selected director query.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,8 +11,17 @@ const {
 const movies = [
   { id: "1", name: "Pulp Fiction", genre: "Crime", directorId: "1" },
   { id: "2", name: "1984", genre: "Sci-Fi", directorId: "2" },
-  { id: 3, name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "3" },
-  { id: 4, name: "Snatch", genre: "Crime-Comedy", directorId: "4" },
+  { id: "3", name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "3" },
+  { id: "4", name: "Snatch", genre: "Crime-Comedy", directorId: "4" },
+  { id: "5", name: "Reservoir Dogs", genre: "Crime", directorId: "1" },
+  { id: "6", name: "The Hateful Eight", genre: "Crime", directorId: "1" },
+  { id: "7", name: "Inglourious Basterds", genre: "Crime", directorId: "1" },
+  {
+    id: "7",
+    name: "Lock, Stock and Two Smoking Barrels",
+    genre: "Crime-Comedy",
+    directorId: "4",
+  },
 ];
 
 const directors = [
@@ -43,6 +52,12 @@ const DirectorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    movies: {
+      type: new graphql.GraphQLList(MovieType),
+      resolve(parent, args) {
+        return movies.filter((movie) => movie.directorId == parent.id);
+      },
+    },
   }),
 });
 
