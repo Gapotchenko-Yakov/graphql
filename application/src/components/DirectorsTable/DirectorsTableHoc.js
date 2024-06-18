23491aedd9cbbf1bb12aad619f238581b7ee5ffa
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-18 10:48:35 +0300

    Display real data from graphql in DirectorsTable.

--- application/src/components/DirectorsTable/DirectorsTableHoc.js
+++ application/src/components/DirectorsTable/DirectorsTableHoc.js
@@ -1,6 +1,8 @@
-import { withStyles } from '@material-ui/core/styles';
-import { compose } from 'recompose';
+import { withStyles } from "@material-ui/core/styles";
+import { compose } from "recompose";
 
-import { styles } from './styles';
+import { styles } from "./styles";
+import { graphql } from "react-apollo";
+import { directorsQuery } from "./queries";
 
-export default compose(withStyles(styles));
+export default compose(withStyles(styles), graphql(directorsQuery));
