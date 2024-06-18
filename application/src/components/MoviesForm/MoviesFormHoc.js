0135dc80c4e907b6feb0d7230721c9fe77a3d18b
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-18 11:30:58 +0300

    With addMovies functionality.

--- application/src/components/MoviesForm/MoviesFormHoc.js
+++ application/src/components/MoviesForm/MoviesFormHoc.js
@@ -1,6 +1,24 @@
-import { withStyles } from '@material-ui/core/styles';
-import { compose } from 'recompose';
+import { withStyles } from "@material-ui/core/styles";
+import { compose } from "recompose";
 
-import { styles } from './styles';
+import { styles } from "./styles";
+import { graphql } from "react-apollo";
+import { addMovieMutation } from "./mutations";
+import { moviesQuery } from "../MoviesTable/queries";
+import { directorsQuery } from "./queries";
 
-export default compose(withStyles(styles));
+const withGraphqlAdd = graphql(addMovieMutation, {
+  props: ({ mutate }) => ({
+    addMovie: (movie) =>
+      mutate({
+        variables: movie,
+        refetchQueries: [{ query: moviesQuery }],
+      }),
+  }),
+});
+
+export default compose(
+  withStyles(styles),
+  withGraphqlAdd,
+  graphql(directorsQuery)
+);
