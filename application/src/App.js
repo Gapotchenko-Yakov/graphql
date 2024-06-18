18507c35fa3fecffece0a0de41339fc0a0b678ba
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-18 10:00:18 +0300

    Connect frontent and backend.

--- application/src/App.js
+++ application/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import Tabs from "./components/Tabs/Tabs";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import theme from "./components/theme";
-import { ApolloClient } from "apollo-boost";
+import ApolloClient from "apollo-boost";
+import { InMemoryCache } from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
   uri: `http://localhost:3005/graphql`,
+  cache: new InMemoryCache(),
 });
 
 class App extends Component {
