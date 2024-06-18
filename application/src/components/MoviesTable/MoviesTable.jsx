8188852ba6b86532ce9cb8b982352c03a0d157aa
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-18 10:40:23 +0300

    Display real data from graphql in MoviesTable.

--- application/src/components/MoviesTable/MoviesTable.jsx
+++ application/src/components/MoviesTable/MoviesTable.jsx
@@ -73,9 +73,9 @@ class MoviesTable extends React.Component {
   render() {
     const { anchorEl, openDialog, data: activeElem = {} } = this.state;
 
-    const { classes } = this.props;
+    const { classes, data = {} } = this.props;
 
-    console.log("this.props.data", this.props.data);
+    const { movies = [] } = data;
 
     return (
       <>
